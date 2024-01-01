9a9de3b7289172c2acc89b817e339e8a25c5dcc9
esxuda <st.sinnerman@gmail.com>
2024-01-02 01:28:47 +0400

    added destroy.php & store.php; some edit; can create and delete notes now

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -7,28 +7,13 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $note = $db->query('select * from notes where id = :id', [
-        'id' => $_GET['id']
-    ])->findOrFail();
+$note = $db->query('select * from notes where id = :id', [
+   'id' => $_GET['id']
+])->findOrFail();
 
-    authorize($note['userId'] === $currentUserId);
+authorize($note['userId'] === $currentUserId);
 
-    $db->query('delete from notes where id = :id', [
-        'id' => $_GET['id']
-    ]);
-
-    header('location: /notes');
-    exit();
-} else {
-    $note = $db->query('select * from notes where id = :id', [
-        'id' => $_GET['id']
-    ])->findOrFail();
-
-    authorize($note['userId'] === $currentUserId);
-
-    view("notes/show.view.php", [
-        'heading' => 'Note',
-        'note' => $note
-    ]);
-}
+view("notes/show.view.php", [
+   'heading' => 'Note',
+   'note' => $note
+]);
