dd247f58ccc55a14fea6dc85259fd42cabed406a
esxuda <st.sinnerman@gmail.com>
2024-01-05 19:06:05 +0400

    tried writing first middleware

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ session_start();
 
 const BASE_PATH = __DIR__.'/../';
 
-require BASE_PATH.'Core/functions.php';
+require BASE_PATH .'Core/functions.php';
 
 spl_autoload_register(function ($class) {
     // Core\Database
